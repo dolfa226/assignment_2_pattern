2776383af7dbfd485d4537996bb77f93d815c9a5
Aira <dolfa226@newschool.edu>
2019-09-12 22:16:01 -0400

    pattern draft

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+	var myWrapper = document.getElementById("rec1"); 
+	
+	for (var i = 0; i < 300; i++) {
+		myWrapper.innerHTML += '<div class="patternUnit" style="background-color: rgb('+ (40 + (i*0)) +',' + (10+ (i*2)) + ',200); transform: rotate('+ (30 * i) +'deg); transform: skewY('+(.3 + i) +'deg);" ></div>';
+
+	}
+
+
+
+
